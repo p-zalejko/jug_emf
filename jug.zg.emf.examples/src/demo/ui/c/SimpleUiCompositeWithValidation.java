8a6c715a285a549e849e52ce7b3b76aa9394af56
Pawel Zalejko <pzalejko@gmail.com>
2015-09-14 16:04:16 +0200

    updated a presentation

---
+++ jug.zg.emf.examples/src/demo/ui/c/SimpleUiCompositeWithValidation.java
@@ -0,0 +1,17 @@
+package demo.ui.c;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.core.databinding.DataBindingContext;
+
+public class SimpleUiCompositeWithValidation extends SimpleUiComposite {
+
+	public SimpleUiCompositeWithValidation(Composite parent, int style) {
+		super(parent, style);
+	}
+	
+	@Override
+	protected DataBindingContext initDataBindings() {
+		return super.initDataBindings();
+	}
+}
+ 
