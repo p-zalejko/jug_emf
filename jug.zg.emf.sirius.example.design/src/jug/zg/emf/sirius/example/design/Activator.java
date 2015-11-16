1a4da4821c8bce7e1b7f8a64172a104579ae0e57
Pawel Zalejko <pzalejko@gmail.com>
2015-11-16 12:05:37 +0100

    little changes to sirius example.

--- jug.zg.emf.sirius.example.design/src/jug/zg/emf/sirius/example/design/Activator.java
+++ jug.zg.emf.sirius.example.design/src/jug/zg/emf/sirius/example/design/Activator.java
@@ -12,55 +12,61 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-    // The plug-in ID
-    public static final String PLUGIN_ID = "jug.zg.emf.sirius.example.design";
+	// The plug-in ID
+	public static final String PLUGIN_ID = "jug.zg.emf.sirius.example.design";
 
-    // The shared instance
-    private static Activator plugin;
+	public static final String MODEL_DIRECTORY = PLUGIN_ID + "/description/";
 
-    private static Set<Viewpoint> viewpoints; 
+	// The shared instance
+	private static Activator plugin;
 
-    /**
-     * The constructor
-     */
-    public Activator() {
-    }
+	private static Set<Viewpoint> viewpoints;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
-    public void start(BundleContext context) throws Exception {
-      super.start(context);
-	  plugin = this;
-	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/demo.odesign")); 
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-	plugin = null;
-	if (viewpoints != null) {
-	    for (final Viewpoint viewpoint: viewpoints) {
-		ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
-	    }
-	    viewpoints.clear();
-	    viewpoints = null; 
+	/**
+	 * The constructor
+	 */
+	public Activator() {
 	}
-	super.stop(context);
-    }
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-	return plugin;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+
+		final ViewpointRegistry instance = ViewpointRegistry.getInstance();
+		viewpoints = new HashSet<Viewpoint>();
+		viewpoints.addAll(instance.registerFromPlugin(MODEL_DIRECTORY + "demo.odesign"));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		if (viewpoints != null) {
+			for (final Viewpoint viewpoint : viewpoints) {
+				ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+			}
+			viewpoints.clear();
+			viewpoints = null;
+		}
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
 }
